c7942a5d45aac0d3a771e583f3bbcd26750d0a45
Kashyap <kashyap.yashvinexus@gmail.com>
2026-04-24 18:04:52 +0530

    Update HomePageClient.jsx

--- components/HomePageClient.jsx
+++ components/HomePageClient.jsx
@@ -25,6 +25,8 @@ export default function HomePageClient() {
 
       <AmenitiesFlip />
 
+      <ProjectCard />
+      
       <section className="section-block">
         <SectionHeading
           eyebrow="Featured Projects"
