57176f02fcfafd0b9ed549ba18d5d44483ee60ee
Kashyap <kashyap.yashvinexus@gmail.com>
2026-04-24 18:10:07 +0530

    Update HomePageClient.jsx

--- components/HomePageClient.jsx
+++ components/HomePageClient.jsx
@@ -8,10 +8,9 @@ import HeroSection from './HeroSection';
 import StatsStrip from './StatsStrip';
 import SampleHouseSlider from './SampleHouseSlider';
 import AmenitiesFlip from './AmenitiesFlip';
-import ScrollOverlapGallery from './ScrollOverlapGallery';
 import SectionHeading from './SectionHeading';
 import ProjectCard from './ProjectCard';
-
+import ScrollOverlapGallery from './ScrollOverlapGallery';
 
 import { featuredProjects, homeGallery } from '@/data/projects';
 
@@ -26,10 +25,8 @@ export default function HomePageClient() {
 
       <AmenitiesFlip />
 
-      <ScrollOverlapGallery />
+      <ScrollOverlapGallery images={homeGallery.slice(0, 5)} />
 
-     
-      
       <section className="section-block">
         <SectionHeading
           eyebrow="Featured Projects"
@@ -49,8 +46,6 @@ export default function HomePageClient() {
           </Link>
         </div>
       </section>
-
-      <ScrollOverlapGallery images={homeGallery.slice(0, 5)} />
 
       <section className="section-block alt-surface">
         <SectionHeading
