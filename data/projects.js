ad43547ee09870519b77e4f4c4beb1a909f60348
Kashyap <kashyap.yashvinexus@gmail.com>
2026-04-27 17:49:24 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -6,7 +6,7 @@ export const stats = [
 ];
 
 export const homeGallery = [
-  'JB_CAM_05_FFF.webp',
+  'img/JB_CAM_05_FFF.webp',
   'ST_CAM_06_FFF.webp',
   'ST_CAM_01_FFF.webp',
   'ST_CAM_02_FFF.webp',
