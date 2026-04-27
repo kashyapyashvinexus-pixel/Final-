cf8e92808f1a71da21eaff98a9ef78fdea2e8938
Kashyap <kashyap.yashvinexus@gmail.com>
2026-04-27 17:47:19 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -6,12 +6,12 @@ export const stats = [
 ];
 
 export const homeGallery = [
-  '/img/JB_CAM_05_FFF.webp',
-  '/img/ST_CAM_06_FFF.webp',
-  '/img/ST_CAM_01_FFF.webp',
-  '/img/ST_CAM_02_FFF.webp',
-  '/img/JB_CAM_16_FFF.webp',
-  '/img/ST_CAM_07_FFF.webp'
+  'JB_CAM_05_FFF.webp',
+  'ST_CAM_06_FFF.webp',
+  'ST_CAM_01_FFF.webp',
+  'ST_CAM_02_FFF.webp',
+  'JB_CAM_16_FFF.webp',
+  'ST_CAM_07_FFF.webp'
 ];
 
 export const projects = [
