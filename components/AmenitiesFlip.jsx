4cfbb47cb161042897575d2a492136369dfc6c58
Kashyap <kashyap.yashvinexus@gmail.com>
2026-04-27 15:42:05 +0530

    Update AmenitiesFlip.jsx

--- components/AmenitiesFlip.jsx
+++ components/AmenitiesFlip.jsx
@@ -21,7 +21,7 @@ export default function AmenitiesFlip() {
         </p>
       </div>
 
-      <div className="amenities-flip-grid">
+      {/*  <div className="amenities-flip-grid">
         {amenities.map((item) => (
           <div className="amenity-card" key={item.title}>
             <img src={item.image} alt={item.title} />
@@ -33,7 +33,7 @@ export default function AmenitiesFlip() {
             </div>
           </div>
         ))}
-      </div>
+      </div>*/}
     </section>
   );
 }
