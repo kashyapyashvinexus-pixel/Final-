27d2250d1dfa61e139dd5b6cec810eec79f12d6e
Kashyap <kashyap.yashvinexus@gmail.com>
2026-05-07 14:10:37 +0530

    Update HomePageClient.jsx

--- components/HomePageClient.jsx
+++ components/HomePageClient.jsx
@@ -10,6 +10,7 @@ import AmenitiesFlip from './AmenitiesFlip';
 import ScrollOverlapGallery from './ScrollOverlapGallery';
 import HeroVideoSection from './HeroVideoSection';
 import ProjectImageSlider from './ProjectImageSlider';
+import StrategicallyConnectedSection from './StrategicallyConnectedSection';
 import ContactBookingSection from './ContactBookingSection';
 
 import { homeGallery } from '@/data/projects';
@@ -30,6 +31,8 @@ export default function HomePageClient() {
       <HeroVideoSection />
 
       <ProjectImageSlider />
+
+      <StrategicallyConnectedSection />
 
       <ContactBookingSection />
     </>
