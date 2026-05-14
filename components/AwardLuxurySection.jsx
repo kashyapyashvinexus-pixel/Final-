e1b3d17a16224f917316ca927020ab7e7782a435
Kashyap <kashyap.yashvinexus@gmail.com>
2026-05-14 16:07:29 +0530

    Add StellaviaAwardSection component

---
+++ components/AwardLuxurySection.jsx
@@ -0,0 +1,50 @@
+export default function StellaviaAwardSection() {
+  return (
+    <section className="stellavia-award-section">
+      <div className="stellavia-award-card">
+        <span className="stellavia-award-diamond stellavia-award-diamond-top"></span>
+        <span className="stellavia-award-diamond stellavia-award-diamond-bottom"></span>
+
+        <div className="stellavia-award-inner">
+          <div className="stellavia-award-images">
+            <div className="stellavia-award-image stellavia-award-image-main">
+              <img
+                src="/logo/Jbp with CM.jpeg"
+                alt="Times Realty Awards Certificate"
+              />
+            </div>
+
+            <div className="stellavia-award-image stellavia-award-image-float">
+              <img
+                src="/award-medal.jpg"
+                alt="Times Realty Awards Ceremony"
+              />
+            </div>
+          </div>
+
+          <div className="stellavia-award-content">
+            <div className="stellavia-award-label-wrap">
+              <span></span>
+              <p>Awards-Winning Excellence</p>
+              <span></span>
+            </div>
+
+            <h2>Times Realty Awards</h2>
+
+            <div className="stellavia-award-title-line">
+              <span></span>
+              <i></i>
+              <span></span>
+            </div>
+
+            <p className="stellavia-award-desc">
+              Stellavia is proud to be honored as the Best Affordable
+              Residential Project by the Honorable Chief Minister of Gujarat,
+              Shri Bhupendra Bhai Patel.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
