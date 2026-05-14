cce24dbf3225b9badd3abed392c7b49f1d25fb2f
Kashyap <kashyap.yashvinexus@gmail.com>
2026-05-14 18:12:10 +0530

    Update award titles and labels in AwardShowcaseSection

--- components/AwardShowcaseSection.jsx
+++ components/AwardShowcaseSection.jsx
@@ -4,12 +4,12 @@ const awards = [
   {
     image: "/logo/Jbp with CM.jpeg",
     label: "Awards-Winning Excellence",
-    title: "Times Realty Awards",
+    title: "Builder Of Nation",
     desc: "Stellavia is proud to be honored as the Best Affordable Residential Project by the Honorable Chief Minister of Gujarat, Shri Bhupendra Bhai Patel.",
   },
   {
     image: "/logo/times property award.webp",
-    label: "Awards-Winning Excellence",
+    label: "Times Property Award",
     title: "Excellence in Architecture Award 2026",
     desc: "Honoring our commitment to innovative design, sustainable building practices, and creating spaces that inspire.",
   },
